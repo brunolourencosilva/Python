3382ef3c5824d8641b37cbbf192e54a2d207bc8b
brunolourencosilva <brunolourenco.silva15@gmail.com>
2024-04-12 21:21:06 -0300

    primeiro programa comitado

---
+++ aula001_primeiro_programa/aula001_hello_word.py
@@ -0,0 +1,18 @@
+#curso de desenvolvimento de sistema
+#autor:Bruno Lourenço
+#data:12/04/2024
+#primeiro programa em python
+
+#importanto biblioteca
+import os
+
+
+#limpando o terminal
+os.system('cls')
+
+#comando print() executa uma saida via terminal
+
+print('-' * 70)
+print('hello word!!!')
+print('-' * 70)
+print()

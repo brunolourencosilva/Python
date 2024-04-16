66bc8d658bfe9ee4ba6b223f8c8c6b6a97d6fbdd
brunolourencosilva <brunolourenco.silva15@gmail.com>
2024-04-15 21:37:25 -0300

    variaveis

---
+++ aula002_variavel/variavel.py
@@ -0,0 +1,30 @@
+import os
+
+
+os.system('cls')
+
+
+print('_'*70)
+print('Estudo de variaveis')
+print('='*70)
+
+
+nome = 'jonh doe'
+nascimento = 1970
+altura = 1.80
+peso = 75.5
+doador = True
+complexo = 3J
+PI = 3.14
+
+
+print('\033[0m \033[31mTipos declarados:\033[0m')
+print('\033[0m A Var \033[32m nome \033[0m é do tipo',type(nome))
+print('\033[0m A Var \033[32m nascimento \033[0m é do tipo',type(nascimento))
+print('\033[0m A Var \033[32m alturA \033[0m é do tipo',type(altura))
+print('\033[0m A Var \033[32m peso \033[0m é do tipo',type(peso))
+print('\033[0m A Var \033[32m doador \033[0m é do tipo',type(doador))
+print('\033[0m A Var \033[32m complexo \033[0m é do tipo',type(complexo))
+print('\033[0m A Var \033[32m PI \033[0m é do tipo',type(PI))
+print('-'*70)
+print('')

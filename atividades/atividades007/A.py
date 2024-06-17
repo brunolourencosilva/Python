23942da0fcbf820f49d6c5432dc911572497fb56
brunolourencosilva <brunolourenco.silva15@gmail.com>
2024-06-16 21:43:04 -0300

    atividade007 - A

--- atividades/atividades007/A.py
+++ atividades/atividades007/A.py
@@ -11,3 +11,14 @@ os.system('cls')
 print('-'*70)
 print('Programa que le um número indeterminado de notas\n(pressione "s" ou "0" para sair)')
 print('='*70)
+
+lista_notas = []
+
+while True:
+    notas = str(input('informe a nota: '))
+    verificação = input('Continuar?(pressione "s" ou "0" para sair): '.lower())
+    lista_notas.append(notas)
+    if verificação == 's':
+        continue
+    else:
+        break

465214c98e05945b3a49900ff7931e9b1b169125
brunolourencosilva <brunolourenci.silva15@mail>
2024-07-04 21:02:11 -0300

    atividade 009

--- atividades/atividades009/C.py
+++ atividades/atividades009/C.py
@@ -1,6 +1,6 @@
 # curso de desenvolvimento de sistema
 # autor:Bruno Lourenço
-# data:1/07/2024
+# data:4/07/2024
 # atividade009 C-Utilizando o exercício anterior , retire um elemento do dicionário.
 
 import os

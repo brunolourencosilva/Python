4d5efd77c88371a57d1c703b236837ddba844994
brunolourencosilva <brunolourenci.silva15@mail>
2024-09-03 18:39:07 -0300

    atividade011 - Letra c completa

--- atividades/atividades011/C.py
+++ atividades/atividades011/C.py
@@ -28,10 +28,11 @@ nota3 = float(input('3ª nota: '))
 nota4 = float(input('4ª nota: '))
 
 resultado = Calcular(nota1, nota2, nota3, nota4)
+media = resultado.media_notas(nota1, nota2, nota3, nota4)
 
 print('-'*70)
 print('-----NOTAS------------')
 print(f'1ª NOTA: {nota1} | 2ª NOTA: {nota2}'
       F'3ª NOTA: {nota3} | 4ª NOTA: {nota4}')
 print('-'*70)
-print(f'MEDIA: {resultado.media_notas(nota1, nota2, nota3, nota4)}')
+print(f'MEDIA: {media}')

3f8a1aa0881ec5ff4137ebcad3d4f30d5c6688db
brunolourencosilva <brunolourenci.silva15@mail>
2024-09-03 19:00:13 -0300

    Atividade011 - Letra E completa

--- atividades/atividades011/E.py
+++ atividades/atividades011/E.py
@@ -1,4 +1,39 @@
 # curso de desenvolvimento de sistema
 # autor:Bruno Lourenço
 # data:02/09/2024
-# atividade0011 
+# atividade0011 E-Faça um programa que receba um número inteiro e mostre o sucessor e antecessor.
+
+import os
+
+
+os.system('cls')
+
+
+class Calcular:
+    def __init__(self, numero):
+        self.numero = numero
+        
+    def sucessor(self,numero):
+        sucessor = numero + 1
+        
+        return sucessor
+    
+    def antecessor(self,numero):
+        adjacentes = numero - 1
+        
+        return adjacentes
+
+# entrada
+print('INFORME UM VALOR')
+numero = int(input('VALOR: '))
+
+resultado = Calcular(numero)
+
+antecessor = resultado.antecessor(numero)
+sucessor = resultado.sucessor(numero)
+
+print('-'*70)
+print('RESULTADO')
+print(f'ANTECESSOR....: {antecessor}')
+print(f'SUCESSOR......: {sucessor}')
+        

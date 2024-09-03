40a6ddc2f929a0e65590168f6fea54c2d7d28e83
brunolourencosilva <brunolourenci.silva15@mail>
2024-09-02 21:20:18 -0300

    atividade011 - letra B completa

--- atividades/atividades011/B.py
+++ atividades/atividades011/B.py
@@ -1,4 +1,29 @@
 # curso de desenvolvimento de sistema
 # autor:Bruno Lourenço
 # data:02/09/2024
-# atividade0011 
+# atividade0011 B-Faça um programa que peça o ano do seu nascimento e calcule a sua idade.
+import os
+import datetime
+
+
+os.system('cls')
+
+class calcular:
+    def __init__(self,ano_nascimento):
+        self.ano_nascimento = ano_nascimento
+        
+    def idade(self,ano_nascimento):
+        ano_atual = datetime.datetime.now().year
+        idade_atual = int(ano_atual) - ano_nascimento
+        
+        return idade_atual
+
+
+print('-'*70)
+print('INFORME SUA IDADE')
+ano_nascimento = int(input('ANO DE NASCIMENTO: '))
+
+resultado = calcular(ano_nascimento)
+
+print('-'*70)
+print(f'VOCE TEM {resultado.idade(ano_nascimento)} ANOS DE IDADE!!!')
--- atividades/atividades011/C.py
+++ atividades/atividades011/C.py
@@ -1,4 +1,4 @@
 # curso de desenvolvimento de sistema
 # autor:Bruno Lourenço
 # data:02/09/2024
-# atividade0011 
+# atividade0011 C-Faça um programa que peça 4 notas, após a entrada de dados calcular a média das notas digitadas.

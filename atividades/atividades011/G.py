dcbc073046e026614eda2e16b9621b2f3f6a3ff8
brunolourencosilva <brunolourenci.silva15@mail>
2024-09-03 19:21:49 -0300

    Atividade011 - Letra G completa

--- atividades/atividades011/G.py
+++ atividades/atividades011/G.py
@@ -1,4 +1,35 @@
 # curso de desenvolvimento de sistema
 # autor:Bruno Lourenço
 # data:02/09/2024
-# atividade0011 
+# atividade0011 G-Faça um programa que converta metros em centímetros e milímetros.
+
+import os
+
+
+os.system('cls')
+
+class Calcular:
+    def __init__(self,metros):
+        self.metros = metros
+    
+    def convertor_centimetros(self,metros):
+        centimetros = metros * 100
+        return centimetros
+    
+    def convertor_milimetros(self,metros):
+        milímetros = metros * 1000
+        return milímetros
+
+print('INFORME UM VALOR EM METROS')
+metros = float(input('VALOR: '))
+
+resultado = Calcular(metros)
+
+centimetros = resultado.convertor_centimetros(metros)
+milimetros = resultado.convertor_milimetros(metros)
+
+print('-'*70)
+print('RESULTADOS')
+print('-'*70)
+print(f'CENTIMETROS....: {centimetros}')
+print(f'MILIMETROS.....: {milimetros}')

e180bcfae975ed887ba5c13a423014ae9ac9f9ab
brunolourencosilva <brunolourenci.silva15@mail>
2024-09-03 20:29:59 -0300

    Atividade011 - Letra I completa

--- atividades/atividades011/I.py
+++ atividades/atividades011/I.py
@@ -1,4 +1,28 @@
 # curso de desenvolvimento de sistema
 # autor:Bruno Lourenço
 # data:02/09/2024
-# atividade0011 
+# atividade0011 I-Faça um programa que receba um valor em reais, depois calcule quantos dólares daria para comprar com esse valor.
+
+import os 
+
+
+os.system('cls')
+
+class Convertor:
+    def __init__(self,real):
+        self.real = real
+        
+    def real_pra_dolar(self,real):
+        dolar = real / 5.65
+        return dolar
+    
+print('-'*70)
+print('INFORME UM VALOR EM REAIS')
+print('-'*70)
+real = float(input('VALOR:'))
+resultado = Convertor(real)
+moeda_convertida = resultado.real_pra_dolar(real)
+
+print('-'*70)
+print('RESULTADOS DE REAL PARA DOLAR')
+print(f'DOLAR....: {moeda_convertida}')

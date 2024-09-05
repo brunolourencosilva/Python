3d89679d7f7f5b29c538ff95087fe55fdd7118a1
brunolourencosilva <brunolourenci.silva15@mail>
2024-09-04 21:26:50 -0300

    Atividade011 - Letra C com validação

--- atividades/atividades011/C.py
+++ atividades/atividades011/C.py
@@ -13,39 +13,34 @@ class Calcular:
         self.nota2 = nota2
         self.nota3 = nota3
         self.nota4 = nota4
-     
-    def media_notas(self,nota1, nota2, nota3, nota4):
-        media = (nota1 + nota2 + nota3 + nota4) / 4
- 
-        return media
     
-   
+    def media_notas(self):
+        try:
+            if float(nota1) < 0 or float(nota2) < 0 or float(nota3) < 0 or float(nota4) < 0:
+                return "Insira valores maiores ou igual a zero"
+            else:
+                media = (float(self.nota1) + float(self.nota2) + float(self.nota3) + float(self.nota4)) / 4
+                return f'A media é: {media}'
+
+        except (TypeError,ValueError):
+            return 'Insira apenas valores numéricos'
+
+
+
 print('-'*70)
 print('INFORME AS NOTAS')
-nota1 = float(input('1ª nota: '))
-nota2 = float(input('2ª nota: '))
-nota3 = float(input('3ª nota: '))
-nota4 = float(input('4ª nota: '))
+nota1 = input('1ª nota: ')
+nota2 = input('2ª nota: ')
+nota3 = input('3ª nota: ')
+nota4 = input('4ª nota: ')
 
-for i in range(0,4):
-    if nota1 < 0 or nota2 < 0 or nota3 < 0 or nota4 < 0:
-        print(f'Nota invalida!!!! ')
-        input('Pressione Enter para continuar')
-        os.system('cls')
-            
-    elif nota1 >= 0 or nota2 >= 0 or nota3 >= 0 or nota4 >= 0:
-            
-        resultado = Calcular(nota1, nota2, nota3, nota4)
-        media = resultado.media_notas(nota1, nota2, nota3, nota4)
-
-        
 resultado = Calcular(nota1, nota2, nota3, nota4)
-media = resultado.media_notas(nota1, nota2, nota3, nota4)
-        
+media = resultado.media_notas()
+
 print('-'*70)
 print('-----NOTAS------------')
-print(f'1ª NOTA: {nota1} | 2ª NOTA: {nota2}'
-    F'3ª NOTA: {nota3} | 4ª NOTA: {nota4}')
+print(f'1ª NOTA: {nota1} | 2ª NOTA: {nota2} | '
+F'3ª NOTA: {nota3} | 4ª NOTA: {nota4}')
 print('-'*70)
-print(f'MEDIA: {media}')
+print(media)
     

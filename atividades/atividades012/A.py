37e85d803f0023ab428b99ba8b176344010c59f9
brunolourencosilva <brunolourenci.silva15@mail>
2024-09-06 19:46:37 -0300

    Atividade012 - Letra A completa

--- atividades/atividades012/A.py
+++ atividades/atividades012/A.py
@@ -3,3 +3,32 @@
 # data:06/09/2024
 # atividade0012 A-Faça um programa que imprima os números no intervalo entre 1 e 100.
 # Os números devem ser apresentados na horizontal.
+
+import os
+
+
+os.system('cls')
+
+class Contador:
+    def __init__(self, inicio, final):
+        self.inicio = inicio
+        self.final = final
+        
+    def intervalo(self, inicio, final):
+        
+        contador = inicio
+        
+        while contador <= final:
+            print(f'numero: {contador}', end=' | ')
+            contador += 1
+
+        return 
+
+inicio = int(input('Digite o valor inicial: '))
+final = int(input('Digite o valor final: '))
+
+objeto_contador = Contador(inicio,final)
+
+intervalo = objeto_contador.intervalo(inicio,final)
+
+print('-'*70)

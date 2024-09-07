4e28893cec51fe429bb0cf8132144c231bd636d2
brunolourencosilva <brunolourenci.silva15@mail>
2024-09-06 21:26:35 -0300

    Atividade012 - Letra G completa

--- atividades/atividades012/H.py
+++ atividades/atividades012/H.py
@@ -3,3 +3,13 @@
 # data:06/09/2024
 # atividade0012 H-Faça um programa que imprima os valores no intervalo definidos pelo usuário.
 # Defina 3 números que deverão ser ignorados, ou seja, que não serão impressos na tela.
+
+class Intervalo:
+    def __init__(self, inicio, final, ignorar1, ignorar2, ignorar3):
+        self.inicio = inicio
+        self.final = final
+        self.ignorar1 = ignorar1
+        self.ignorar2 = ignorar2
+        self.ignorar3 = ignorar3
+    
+        

43b07dc2bfb6070ec3a4997207733635c38ba004
brunolourencosilva <brunolourenci.silva15@mail>
2024-09-09 19:11:07 -0300

    Atividade012 - Letra H completa

--- atividades/atividades012/H.py
+++ atividades/atividades012/H.py
@@ -3,33 +3,50 @@
 # data:06/09/2024
 # atividade0012 H-Faça um programa que imprima os valores no intervalo definidos pelo usuário.
 # Defina 3 números que deverão ser ignorados, ou seja, que não serão impressos na tela.
+import os
 
-class Intervalo:
+
+os.system('cls')
+
+
+class Exibir:
     def __init__(self, inicio, final, ignorar1, ignorar2, ignorar3):
         self.inicio = inicio
         self.final = final
         self.ignorar1 = ignorar1
         self.ignorar2 = ignorar2
         self.ignorar3 = ignorar3
-    
-    def exibir_ignorados(self, ignorar1, ignorar2, ignorar3):
-        print(f'Numeros ignorados: {ignorar1} | {ignorar2} | {ignorar3}')
 
-class (Intervalo):
-    
-    def intervalo(self, inicio, final):
-        
-        contador = inicio
-        for contador in range(final + 1):
-            print(f'{contador}', end=' | ')
+    def exibir_ignorados(self):
+        print('-'*70)
+        print(f'Numeros ignorados: {self.ignorar1} | {self.ignorar2} | {self.ignorar3}')
+        print('-'*70)
+
+
+class Ignora(Exibir):
+
+    def intervalo_ignorando(self):
+
+        # declaração
+        contador = self.inicio
+
+        # saida
+        while contador <= self.final:
+            # ignorando os números especificados
+            if contador != self.ignorar1 and contador != self.ignorar2 and contador != self.ignorar3:
+                print(contador, end=' | ')
             contador += 1
 
-
+print('='*70)
 inicio = int(input('Digite o valor inicial: '))
 final = int(input('Digite o valor final: '))
+print('-'*70)
+ignorar1 = int(input("Digite o 1º número que sera ignorado: "))
+ignorar2 = int(input("Digite o 2º número que sera ignorado: "))
+ignorar3 = int(input("Digite o 3º número que sera ignorado: "))
 
-objeto_contador = Intervalo(inicio,final)
+exibindo = Exibir(inicio, final, ignorar1, ignorar2, ignorar3)
+exibindo.exibir_ignorados()
 
-intervalo = objeto_contador.intervalo(inicio,final)
-
-        
+intervalo = Ignora(inicio, final, ignorar1, ignorar2, ignorar3)
+intervalo.intervalo_ignorando()

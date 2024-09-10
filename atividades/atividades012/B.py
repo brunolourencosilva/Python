76e948dde5553bef10748fc8fd833e05e5dbe5a2
brunolourencosilva <brunolourenci.silva15@mail>
2024-09-10 19:15:44 -0300

    Atividade012 - Letra B completa e comentada

--- atividades/atividades012/B.py
+++ atividades/atividades012/B.py
@@ -8,24 +8,27 @@ import os
 
 os.system('cls')
 
+# Define a classe 'InicioFim' que irá armazenar os valores de início e final do intervalo
 class InicioFim:
     def __init__(self, inicio, final):
         self.inicio = inicio
         self.final = final
 
+# Define a classe Intervalo que herda atributos da classe InicioFim
 class Intervalo(InicioFim):
     
+    # Método para exibir os números dentro do intervalo
     def intervalo(self, inicio, final):
         
+        # Usa um loop for para emprimir o intervalo
         contador = inicio
-        for contador in range(final + 1):
+        for contador in range(inicio,final + 1):
             print(f'{contador}', end=' | ')
             contador += 1
-
 
 inicio = int(input('Digite o valor inicial: '))
 final = int(input('Digite o valor final: '))
 
+# Cria um objeto da classe Intervalo pegando os valores de inicio e final
 objeto_contador = Intervalo(inicio,final)
-
 intervalo = objeto_contador.intervalo(inicio,final)

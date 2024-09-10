a48e6a11ba278a1b16c10cc2ff58e749d0599bb1
brunolourencosilva <brunolourenci.silva15@mail>
2024-09-10 20:35:18 -0300

    Atividade012 - Letra C completo e comentado

--- atividades/atividades012/C.py
+++ atividades/atividades012/C.py
@@ -8,14 +8,16 @@ import os
 
 os.system('cls')
 
-
+# Define a classe InicioFim que irá armazenar os valores de início e final do intervalo
 class InicioFim:
     def __init__(self, inicio, final):
         self.inicio = inicio
         self.final = final
 
+# Define a classe Intervalo_invertido que herda atributos da classe InicioFim
 class Intervalo_invertido(InicioFim):
     
+    # Método para exibir os números dentro do intervalo na ordem decrescente
     def intervalo(self, inicio, final):
         
         contador = inicio
@@ -28,6 +30,6 @@ print('-'*70)
 print('Programa que imprima os números no intervalo entre 1 e 10 em ordem decrescente')
 print('='*70) 
 
-
+# Cria um objeto da classe Intervalo_invertido pegando os valores de inicio e final
 objeto_contador = Intervalo_invertido(0,10)
 intervalo = objeto_contador.intervalo(0,10)

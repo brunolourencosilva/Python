6629118a23f323a5ed2678ab7332712e0c71a0c4
brunolourencosilva <brunolourenci.silva15@mail>
2024-09-13 19:56:18 -0300

    comentarios para ajudar no entendimento do codigo

--- testes/cadastro.py
+++ testes/cadastro.py
@@ -3,19 +3,22 @@ import os
 
 os.system('cls')
 
-
+# Criando a classe pai
 class ClassePai:
-    def __init__(self,nome,idade,ano_nascimento):
+    def __init__(self,nome,idade,ano_nascimento): # Metodo construtor
+        # Definindo atributos
         self.nome = nome
         self.idade = idade
         self.ano_nascimento = ano_nascimento
 
-class ClasseFilha(ClassePai):
+# Criando a classe filha
+class ClasseFilha(ClassePai): # Classe filha herdando atributos da classe pai
     def __init__(self,nome,idade,ano_nascimento):
         self.nome = nome
         self.idade = idade
         self.ano_nascimento = ano_nascimento
     
+    # Definindo metodos
     def cadastro(self):
         lista = {}
         
@@ -29,5 +32,8 @@ nome = str(input('Nome: '))
 idade = int(input('Idade: '))
 ano_nascimento = int(input('Ano de nascimento: '))
 print('-'*70)
-exibir = ClasseFilha(nome,idade,ano_nascimento)
+
+exibir = ClasseFilha(nome,idade,ano_nascimento) # Criando um objeto da classe filha
+
+# Usando o objeto Exibir(que é uma objeto da classe filha) para chamar o metodo Cadastro da classe filha
 exibir.cadastro()
